982e5110f8889c28eabc31e7144e23ecb52ecf8e
keithit110 <keith5132007@hotmail.com>
2021-05-22 17:33:28 -0400

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,140 @@
-let js='fun';
-        if (js==='fun') alert ('JS is so fun');
-        let x = 1 + 1 + 1 + 10;
-        //alert (x);
-        console.log (x);
+// let js='fun123';
+//         if (js==='fun') alert ('JS is so fun');
+//         let x = 11 + 1 + 1 + 10;
+//         //alert (x);
+//         console.log (x);
+        
+//         console.log (js);
+//         let PI;
+//         console.log(PI);
+
+//         //variable assignment
+//         let country = 'Philippines', population = '105m', continent = 'Asia';
+
+//         console.log(continent, '-',country, population);
+        
+//         //end variable assignment
+//         let y = true;
+//         console.log(y);
+
+//         console.log(typeof y);
+
+//         char = 'no';
+//         console.log (char);
+        
+//         console.log(typeof null);
+
+// //data type assignment
+// let isIsland = true;
+// let language;
+
+// console.log('variable assignment');
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+// //end data type assignment
+
+// const ageJonas = 2038-1991;
+// const ageSarah = 2038-2010;
+// console.log(ageJonas/2);
+// const fName='Jonas';
+// const lName='Rivera';
+// const space=' ';
+// console.log(fName+space+lName);
+// console.log( ageJonas > ageSarah);
+// console.log(5-10);
+
+// const markWeight = 78, markHeight=1.69, johnWeight=92, johnHeight=1.95;
+// const bmiMark= markWeight / markHeight ** 2, bmiJohn=johnWeight/johnHeight**2;
+// const markHigherBMI =bmiMark>bmiJohn;
+
+// console.log(bmiMark,bmiJohn);
+// console.log(markHigherBMI);
+
+// if(markHigherBMI== true){
+//         console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})`);
+// }else console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})`)
+
+// console.log('string \n\
+// multiple \n\
+//         d')
+
+
+// console.log(`string
+// with
+//         d`);
+
+
+
+// if (1!=1){
+//         console.log('if');
+// }
+// else if (1!=2){
+//         console.log('else if');
+// }
+// else console.log('else');
+
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+
+// const money=Number(prompt('how much is your money?'));
+
+// if(money== 12){
+//     console.log('1st if block');
+// } else if (money =='10'){
+//     console.log('2nd if block')
+// } else {
+//     console.log('else block');
+// }
+
+// const hasDriversLicense=true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && !hasGoodVision);
+
+// const scoreDolphin=(97+112+101)/3;
+// const scoreKoalas=(109+95+123)/3;
+
+// console.log(`D: ${scoreDolphin}
+// K: ${scoreKoalas}`)
+
+// if(scoreDolphin>scoreKoalas && scoreDolphin >=100){
+//     console.log('Dolphin wins!');
+// } else if(scoreDolphin<scoreKoalas && scoreKoalas >=100){
+//     console.log('Koala wins!')
+// } else if (scoreDolphin < 100 && scoreKoalas < 100){
+//     console.log('no one wins!');
+// } else{
+//     if(scoreDolphin===scoreKoalas && (scoreKoalas>= 100 && scoreDolphin >= 100))
+//     console.log('Its a draw!');
+//     else
+//     console.log('no one wins');
+// }
+
+// const day='Friday';
+
+// if(day==='monday'){
+//     console.log('its monday')
+// } else if(day==='tuesday'){
+//     console.log('its tuesday');
+// } else if(day==='wednesday' || day==='thursday'){
+//     console.log('its wedthurs');
+// } else if (day==='friday'){
+//     console.log('its friday');
+// } else if (day==='saturday' || day==='sunday'){
+//     console.log('its satsun');
+// } else {
+//     console.log('not a valid day');
+// }
+    
+
+// const age = '12';
+// age>=18 ? console.log('18+') : console.log('-18');
+
+
+
+// const bill=430;
+// const tip=(bill<=300 && bill>=50) ? bill*.15 : bill*.2;
+
+// console.log(`Bill value is ${bill}, tip is ${tip}, total value to pay is ${bill+tip}`);
